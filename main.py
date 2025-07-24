a5ad96cb84efe4fe23cb115bbc8d40eb40d39da7
Vansh <vanshayushi@gmail.com>
2025-07-25 04:06:40 +0530

    origins update

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",         
     "https://budget-buddy-693559507420.asia-south1.run.app/",   
+    "https://budget-buddy-909236466645.asia-south1.run.app",
+    "https://budget-buddy-weld.vercel.app/",
 ]
 
 app.add_middleware(
